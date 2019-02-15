c175a756595e2ea8804cb15b8be67e32e4c8bb7c
Jingwen Zhang <43404438+Jw-Jn@users.noreply.github.com>
2019-02-14 16:31:47 -0800

    Add files via upload

--- model.py
+++ model.py
@@ -7,18 +7,19 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
         # todo
         # Hint: Do not use ReLU in last convolutional set of up-path (128-64-64) for stability reasons!
-        self.downStep1 = downStep(1, 64, bn=bn)
+        self.downStep1 = downStep(1, 64, firstLayer=True, bn=bn)
         self.downStep2 = downStep(64, 128, bn=bn)
         self.downStep3 = downStep(128, 256, bn=bn)
         self.downStep4 = downStep(256, 512, bn=bn)
-        self.downStep5 = downStep(512, 1024, lastLayer=True, bn=bn)
+        self.downStep5 = downStep(512, 1024, bn=bn)
         
         self.upStep1 = upStep(1024, 512, bn=bn)
         self.upStep2 = upStep(512, 256, bn=bn)
         self.upStep3 = upStep(256, 128, bn=bn)
         self.upStep4 = upStep(128, 64, withReLU=False, bn=bn)
 
-        self.conv1 = nn.Conv2d(64, n_classes, 1)
+        self.conv = nn.Conv2d(64, n_classes, 1)
+        self.dropout = nn.Dropout(0.4)
 
     def forward(self, x):
         # todo
@@ -26,46 +27,55 @@ class UNet(nn.Module):
         x2 = self.downStep2(x1)
         x3 = self.downStep3(x2)
         x4 = self.downStep4(x3)
-        x5 = self.downStep5(x4)
+        # x4 = self.dropout(x4)
+        x5 = self.downStep5(x4) 
+        
 
-        x = self.up1(x5, x4)
-        x = self.up1(x, x3)
-        x = self.up1(x, x2)
-        x = self.up1(x, x1)
+        x = self.upStep1(x5, x4)
+        # print(x.shape)
+        x = self.upStep2(x, x3)
+        # print(x.shape)
+        x = self.upStep3(x, x2)
+        # print(x.shape)
+        x = self.upStep4(x, x1)
+        # print(x.shape)
 
-        x = self.conv1(x)
+        x = self.conv(x)
+        # print(x.shape)
 
         return x
 
 class downStep(nn.Module):
-    def __init__(self, inC, outC, lastLayer=False, bn=False):
+    def __init__(self, inC, outC, firstLayer=False, bn=False):
         super(downStep, self).__init__()
         # todo
+        self.bn = bn
+        self.firstLayer = firstLayer
         self.conv_bn = nn.Sequential(
-            nn.Conv2d(inC, outC, 3),
+            nn.Conv2d(inC, outC, 3, padding=0),
             nn.BatchNorm2d(outC),
             nn.ReLU(),
-            nn.Conv2d(outC, outC, 3),
+            nn.Conv2d(outC, outC, 3, padding=0),
             nn.BatchNorm2d(outC),
             nn.ReLU())
 
-        self.conv_bn = nn.Sequential(
-            nn.Conv2d(inC, outC, 3),
+        self.conv = nn.Sequential(
+            nn.Conv2d(inC, outC, 3, padding=0),
             nn.ReLU(),
-            nn.Conv2d(outC, outC, 3),
+            nn.Conv2d(outC, outC, 3, padding=0),
             nn.ReLU())
 
-        self.maxpool = nn.MaxPool2d(2, stride=2)
+        self.maxpool = nn.MaxPool2d(2)
 
     def forward(self, x):
         # todo
+        if not self.firstLayer:
+            x = self.maxpool(x)
+
         if self.bn:
             x = self.conv_bn(x)
         else:
             x = self.conv(x)
-
-        if not self.lastLayer:
-            x = self.maxpool(x)
 
         return x
 
@@ -75,43 +85,45 @@ class upStep(nn.Module):
         # todo
         # Do not forget to concatenate with respective step in contracting path
         # Hint: Do not use ReLU in last convolutional set of up-path (128-64-64) for stability reasons!
+        self.bn = bn
+        self.withReLU = withReLU
         self.conv_bn_relu = nn.Sequential(
-            nn.Conv2d(inC, outC, 3),
+            nn.Conv2d(inC, outC, 3, padding=0),
             nn.BatchNorm2d(outC),
             nn.ReLU(),
-            nn.Conv2d(outC, outC, 3),
+            nn.Conv2d(outC, outC, 3, padding=0),
             nn.BatchNorm2d(outC),
             nn.ReLU())
 
         self.conv_bn = nn.Sequential(
-            nn.Conv2d(inC, outC, 3),
+            nn.Conv2d(inC, outC, 3, padding=0),
             nn.BatchNorm2d(outC),
-            nn.Conv2d(outC, outC, 3),
+            nn.Conv2d(outC, outC, 3, padding=0),
             nn.BatchNorm2d(outC))
 
         self.conv_relu = nn.Sequential(
-            nn.Conv2d(inC, outC, 3),
+            nn.Conv2d(inC, outC, 3, padding=0),
             nn.ReLU(),
-            nn.Conv2d(outC, outC, 3),
+            nn.Conv2d(outC, outC, 3, padding=0),
             nn.ReLU())
 
         self.conv = nn.Sequential(
-            nn.Conv2d(inC, outC, 3),
-            nn.Conv2d(outC, outC, 3))
+            nn.Conv2d(inC, outC, 3, padding=0),
+            nn.Conv2d(outC, outC, 3, padding=0))
 
-        self.upsampling = nn.UpsamplingBilinear2d(2)
+        self.upsampling = nn.ConvTranspose2d(inC, outC, 2, 2)
 
     def forward(self, x, x_down):
         # todo
         x = self.upsampling(x)
         
         # input is CHW
-        p = x_down.size() - x.size()
+        py = x_down.size()[2] - x.size()[2]
+        px = x_down.size()[3] - x.size()[3]
 
-        x = F.pad(x, (p[3] // 2, p[3] - p[3]//2,
-                        p[2] // 2, p[2] - p[2]//2))
-        
-        x = torch.cat([x_down, x], dim=1)
+        x_down = x_down[:, :, py//2:x_down.shape[2]-py//2, px//2:x_down.shape[3]-px//2]
+
+        x = torch.cat([x_down, x], dim=1) #512, 1024
 
         if self.withReLU and self.bn: # not last layer, with bn
             x = self.conv_bn_relu(x)
--- train.py
+++ train.py
@@ -9,99 +9,125 @@ import torch.backends.cudnn as cudnn
 import torch.nn as nn
 from torch import optim
 from torch.autograd import Variable
+import torch.nn.functional as F
 
 from torchvision import transforms
 
 import matplotlib.pyplot as plt
+# from PIL import Image, ImageOps
 
 from model import UNet
 from dataloader import DataLoader
+
+import time
 
 def train_net(net,
               epochs=5,
               data_dir='data/cells/',
               n_classes=2,
-              lr=0.1,
+              lr=0.001,
               val_percent=0.1,
               save_cp=True,
-              gpu=False):
-    loader = DataLoader(data_dir)
+              gpu=False,
+              train=True,
+              pth_dir='./data/cells/checkpoints/CP10.pth',
+              flip=False,
+              rotate=False,
+              zoom=False,
+              gamma_c=False,
+              deform=False):
+
+    loader = DataLoader(data_dir, flip=flip, rotate=rotate, gamma_c=gamma_c, zoom=zoom, deform=deform)
 
     N_train = loader.n_train()
  
-    optimizer = optim.SGD(net.parameters(),
-                            lr=lr,
-                            momentum=0.99,
-                            weight_decay=0.0005)
-
-    for epoch in range(epochs):
-        print('Epoch %d/%d' % (epoch + 1, epochs))
-        print('Training...')
-        net.train()
+    optimizer = optim.SGD(net.parameters(), lr=lr, momentum=0.99, weight_decay=0.0005)
+    
+    if train:
         loader.setMode('train')
 
-        epoch_loss = 0
+        for epoch in range(epochs):
+            print('Epoch %d/%d' % (epoch + 1, epochs))
+            print('Training...')
+            net.train()
 
-        for i, (img, label) in enumerate(loader):
-            shape = img.shape
-            label = label - 1
-            # todo: create image tensor: (N,C,H,W) - (batch size=1,channels=1,height,width)
-            img = torch.from_numpy(img)
-            label = torch.from_numpy(label)
+            epoch_loss = 0
 
-            img = img.permute(0, 3, 2, 1).contiguous() # [batch_size, W, H, CH] -> [batch_size, CH, H, W]
-            label = label.permute(0, 3, 2, 1).contiguous()
+            start = time.process_time()
 
-            # todo: load image tensor to gpu
-            if gpu:
-                img = img.cuda()
-                label = label.cuda()
+            for i, (img, label) in enumerate(loader):
+                shape = img.shape
+                label = label - 1
+                # todo: create image tensor: (N,C,H,W) - (batch size=1,channels=1,height,width)
+                # img = Image.fromarray(img*255.)
+                # img.show()
+                
+                img = torch.from_numpy(img)
+                img = img.view(1, 1, shape[0], shape[1])
 
-            img = Variable(img)
-            label = Variable(label)
+                # todo: load image tensor to gpu
+                if gpu:
+                    img = Variable(img.cuda())
 
-            optimizer.zero_grad()
+                # todo: get prediction and getLoss()
+                pred_label = net(img)
 
-            # todo: get prediction and getLoss()
-            pred_label = net(img)
+                loss = getLoss(pred_label, label)
 
-            pred_label = pred_label.view(-1)
-            target_label = label.view(-1)
+                epoch_loss += loss.item()
+    
+                # print('Training sample %d / %d - Loss: %.6f' % (i+1, N_train, loss.item()))
 
-            loss = getLoss(pred_label, target_label)
+                # optimize weights
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
-            epoch_loss += loss.item()
- 
-            print('Training sample %d / %d - Loss: %.6f' % (i+1, N_train, loss.item()))
+            if (epoch+1)==40 or (epoch+1)==50 or (epoch+1)==60 or (epoch+1)==70:
+                torch.save(net.state_dict(), join(data_dir, 'checkpoints') + '/CP%d.pth' % (epoch + 1))
+                print('Checkpoint %d saved !' % (epoch + 1))
+            print('Epoch %d finished! - Loss: %.6f' % (epoch+1, epoch_loss / i))
+            print('Time: ', time.process_time() - start)
 
-            # optimize weights
-            loss.backward()
-            optimizer.step()
-
-        torch.save(net.state_dict(), join(data_dir, 'checkpoints') + '/CP%d.pth' % (epoch + 1))
-        print('Checkpoint %d saved !' % (epoch + 1))
-        print('Epoch %d finished! - Loss: %.6f' % (epoch+1, epoch_loss / i))
-
+    
+    
     # displays test images with original and predicted masks after training
-    loader.setMode('test')
-    net.eval()
-    with torch.no_grad():
-        for _, (img, label) in enumerate(loader):
-            shape = img.shape
-            img_torch = torch.from_numpy(img.reshape(1,1,shape[0],shape[1])).float()
-            if gpu:
-                img_torch = img_torch.cuda()
-            pred = net(img_torch)
-            pred_sm = softmax(pred)
-            _,pred_label = torch.max(pred_sm,1)
+    else:
+        print('Testing')
+        loader.setMode('test')
+        net_state = torch.load(pth_dir)
+        net.load_state_dict(net_state)
+        net.eval()
+        with torch.no_grad():
+            for i, (img, label) in enumerate(loader):
+                shape = img.shape
+                img_torch = torch.from_numpy(img.reshape(1,1,shape[0],shape[1])).float()
+                if gpu:
+                    img_torch = img_torch.cuda()
+                pred = net(img_torch)
+                pred_sm = softmax(pred)
+                _,pred_label = torch.max(pred_sm,1)
 
-            plt.subplot(1, 3, 1)
-            plt.imshow(img*255.)
-            plt.subplot(1, 3, 2)
-            plt.imshow((label-1)*255.)
-            plt.subplot(1, 3, 3)
-            plt.imshow(pred_label.cpu().detach().numpy().squeeze()*255.)
-            plt.show()
+                pad = (572-388)//2
+                img = img[pad:572-pad, pad:572-pad]
+                plt.subplot(1, 3, 1)
+                plt.imshow(img*255.)
+                plt.subplot(1, 3, 2)
+                plt.imshow((label-1)*255.)
+                plt.subplot(1, 3, 3)
+
+                plt.imshow(pred_label.cpu().detach().numpy().squeeze()*255.)
+                # plt.show()
+                plt.savefig('test_'+str(i)+'.png')
+
+                # img = Image.fromarray(img*255.)
+                # img.show()
+                # label = Image.fromarray((label-1)*255.)
+                # label.show()
+                # pred_label = pred_label.cpu().detach().numpy().squeeze()*255.
+                # pred_label = Image.fromarray(pred_label*255.)
+                # pred_label.show()
+            
 
 def getLoss(pred_label, target_label):
     p = softmax(pred_label)
@@ -109,19 +135,21 @@ def getLoss(pred_label, target_label):
 
 def softmax(input):
     # todo: implement softmax function
-    p = nn.Softmax(input) #?
+    p = torch.exp(input.float()) / torch.sum(torch.exp(input.float()), dim=1)
+    # print(p)
     return p
 
 def cross_entropy(input, targets):
     # todo: implement cross entropy
     # Hint: use the choose function
-    ce = choose(input, targets)
-
+    pred = choose(input, targets)
+    pixel = pred.size()[0]*pred.size()[1]
+    ce = -torch.sum(torch.log(pred)) / pixel
     return ce
 
 # Workaround to use numpy.choose() with PyTorch
 def choose(pred_label, true_labels):
-    size = pred_label.size()
+    size = pred_label.size()   # (N,C,H,W)
     ind = np.empty([size[2]*size[3],3], dtype=int)
     i = 0
     for x in range(size[2]):
@@ -129,7 +157,7 @@ def choose(pred_label, true_labels):
             ind[i,:] = [true_labels[x,y], x, y]
             i += 1
 
-    pred = pred_label[0,ind[:,0],ind[:,1],ind[:,2]].view(size[2],size[3])
+    pred = pred_label[0, ind[:,0], ind[:,1], ind[:,2]].view(size[2],size[3])
 
     return pred
     
@@ -139,7 +167,11 @@ def get_args():
     parser.add_option('-c', '--n-classes', dest='n_classes', default=2, type='int', help='number of classes')
     parser.add_option('-d', '--data-dir', dest='data_dir', default='data/cells/', help='data directory')
     parser.add_option('-g', '--gpu', action='store_true', dest='gpu', default=False, help='use cuda')
-    parser.add_option('-l', '--load', dest='load', default=False, help='load file model')
+    parser.add_option('--test', action='store_false', default=True, help='testing mode')
+    parser.add_option('--pth', default='./data/cells/checkpoints/CP60.pth', help='pth directory')
+    parser.add_option('--frz', action='store_true', default=False, help='flip rotate zoom')
+    parser.add_option('--gamma', action='store_true', default=False, help='gamma correction')
+    parser.add_option('--deform', action='store_true', default=False, help='deformation')
 
     (options, args) = parser.parse_args()
     return options
@@ -147,11 +179,7 @@ def get_args():
 if __name__ == '__main__':
     args = get_args()
 
-    net = UNet(n_classes=args.n_classes)
-
-    if args.load:
-        net.load_state_dict(torch.load(args.load))
-        print('Model loaded from %s' % (args.load))
+    net = UNet(n_classes=args.n_classes, bn=True)
 
     if args.gpu:
         net.cuda()
@@ -161,4 +189,12 @@ if __name__ == '__main__':
         epochs=args.epochs,
         n_classes=args.n_classes,
         gpu=args.gpu,
-        data_dir=args.data_dir)
+        data_dir=args.data_dir,
+        train=args.test,
+        pth_dir=args.pth,
+        flip=args.frz,
+        rotate=args.frz,
+        zoom=args.frz,
+        gamma_c=args.gamma,
+        deform=args.deform)
+
